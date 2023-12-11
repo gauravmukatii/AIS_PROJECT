9def613989d6745e88241738db7c76d79e547d94
Gaurav Mukati <gaurav.mukati99@gmail.com>
2023-12-11 14:52:02 +0530

    COMMIT

---
+++ src/main/java/in/springboot/AIT_PROJECT/controller/IndexController.java
@@ -0,0 +1,13 @@
+package in.springboot.AIT_PROJECT.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String IndexPage(){
+        return "index";
+    }
+}
